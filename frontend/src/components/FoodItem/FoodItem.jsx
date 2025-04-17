6e06022bbb7cef41eeeb8b58078fa51f8921bfad
Siffat-Easha <siffat.ara.easha@g.bracu.ac.bd>
2025-04-18 01:09:36 +0600

    Easha's first commit

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './FoodItem.css'
+
+const FoodItem = ({id,name,price,description,image}) => {
+  return (
+    <div className='food-item'>
+        <div className='food-item-img-container'>
+
+            <img className='food-item-imge' src={image} alt=''/>
+
+        </div>
+        <div className='food-item-info'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default FoodItem
